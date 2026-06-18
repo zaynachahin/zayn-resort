624d934949bfacdeaa6887c10660e382a292dfd3
Zayn Chahin <zaynachahin@gmail.com>
2026-06-17 22:57:18 -0300

    feat: get customer by id endpoint

--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
-from app.repositories.customer_repository import list_customers
+from fastapi import FastAPI, HTTPException
+
+from app.repositories.customer_repository import (
+    list_customers, 
+    get_customer_by_uuid
+)
+
+from uuid import UUID
 
 app = FastAPI()
+
 
 @app.get("/customers")
 def get_customers():
     return list_customers()
+
+
+@app.get("/customers/{customer_id}")
+def get_customer(customer_id: UUID):
+    customer = get_customer_by_uuid(customer_id)
+
+    if not customer:
+        raise HTTPException(
+            status_code=404, 
+            detail= "Customer not found"
+            )
+    
+    return customer[0]
--- app/repositories/customer_repository.py
+++ app/repositories/customer_repository.py
@@ -8,3 +8,16 @@ def list_customers():
 
     customers = execute_query(query, fetch=True)
     return customers
+
+
+def get_customer_by_uuid(customer_id):
+    query = """
+    SELECT id, full_name, date_of_birth, cpf, newsletter_opt_in 
+    FROM customers
+    WHERE id = %s;
+    """
+
+    params = (str(customer_id),)
+
+    customer = execute_query(query, params, fetch=True)
+    return customer
